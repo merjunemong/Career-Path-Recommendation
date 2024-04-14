95ee398646331f0b8e32a308ea3ae2a1aadea376
merjunemong <kdjun016@naver.com>
2024-04-14 21:07:23 +0900

    new file:   pre_processed_data/DataInfo.md 	new file:   pre_processed_data/Pre_pocessed-Job-Classification.csv 	new file:   pre_processed_data/image.png 	new file:   pre_processed_data/ncs-info.csv 	new file:   pre_processed_data/ncs-to-keco.csv 	new file:   pre_processed_data/pre_processing_csv.py 	modified:   "\355\232\214\354\235\230\353\241\235/04_08_\355\232\214\354\235\230\353\241\235"

---
+++ pre_processed_data/pre_processing_csv.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+with open('직업세세분류.csv') as inputfile:
+    df = pd.read_csv(inputfile, encoding='utf-8')
+
+data = []
+
+for index, row in df.iterrows():
+    code = str(row['KNOW직업대분류']) + str(row['KNOW직업중분류']) + str(row['KNOW직업소분류']) + str(row['KNOW직업세분류'])
+    data.append([code, row['KNOW직업세세분류'], row['KNOW직업명']])
+
+new_df = pd.DataFrame(data, columns=['KECO', 'subNum', 'name'])
+
+new_df.to_csv('Pre_pocessed-Job-Classification.csv', index=False, encoding='utf-8-sig')
+
+
+with open('한국산업인력공단_국가직무능력표준 정보_20231129.CSV') as inputfile:
+    df = pd.read_csv(inputfile, encoding='utf-8')
+
+data = []
+
+for index, row in df.iterrows():
+    code = str(row['분류번호']).split('_')[0]
+    data.append([code, row['명칭'], row['수준']])
+
+new_df = pd.DataFrame(data, columns=['ncsClCd', 'compeUnitName', 'level'])
+
+new_df.to_csv('ncs-info.csv', index=False, encoding='utf-8-sig')
+
+
+with open('NCS_KECO_연계표.CSV') as inputfile:
+    df = pd.read_csv(inputfile, encoding='utf-8')
+
+data = []
+
+for index, row in df.iterrows():
+    code = str(row['대분류(NCS)']).split('.')[0] + str(row['중분류(NCS)']).split('.')[0] + str(row['소분류(NCS)']).split('.')[0] + str(row['세분류(NCS)']).split('.')[0]
+    data.append([code, row['코드(KECO)'], row['세분류(KECO)']])
+
+new_df = pd.DataFrame(data, columns=['ncsClCd', 'KECO', 'name'])
+
+print(new_df)
+
+new_df.to_csv('ncs-to-keco.csv', index=False, encoding='utf-8-sig')
+
+with open('ncs-to-keco.CSV', encoding='utf-8-sig') as inputfile:
+    df = pd.read_csv(inputfile)
+    print(df)
